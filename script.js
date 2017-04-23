79a9a62b7685c75ba040d8ec30c5225345ceb572
Tor Steenson <v.steenson@ymail.com>
2017-04-23 15:56:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+  $.getJSON(url, getQuote);
+  getColor(colors);
+
+
+    $("#newQuote").on('click', function() {
+      $.getJSON(url, getQuote);
+      getColor(colors);
+      console.log('hello');
+    });
+});
+
+
+var url = "https://random-quote-generator.herokuapp.com/api/quotes/random";
+
+var colors = ['#2980b9', '#e74c3c', '#e67e22', '#27ae60', '#f1c40f', '#1abc9c', '#95a5a6', '#8e44ad', "#16a085", "#d35400", "#f39c12", "#3498db"];
+
+var getQuote = function (data) {
+  $("#text").text(" " + data.quote);
+  
+  if (data.author === '') {
+    data.author = 'Unknown';
+  }
+  
+  $("#author").text("- " + data.author);
+  
+var quot = 'https://twitter.com/intent/tweet?text=' + data.author + 'Author ' + data.quote +' @terrifictor codepen.io/terrifictor100';
+  
+  $(".twitter-share").attr("href", quot);
+  
+  
+  
+}
+
+var getColor = function(){
+    var randomColor = Math.floor(Math.random() * (colors.length));
+    $("body").animate({"background-color": colors[randomColor], "color":colors[randomColor]}, 1000);
+
+    $(".btn").animate({"border-color": colors[randomColor], "color":colors[randomColor]});
+
+    $(".twitter-share").animate({ "color":colors[randomColor]}, 1000);
+
+  
+   }
+ 
+
+
