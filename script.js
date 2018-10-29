d94f500333cee7081c7d22b101114b6112b9eb2f
Tor Steenson <v.steenson@ymail.com>
2018-10-29 16:43:43 +0000

    Update script.js

--- script.js
+++ script.js
@@ -11,20 +11,20 @@ $(document).ready(function() {
 });
 
 
-var url = "https://random-quote-generator.herokuapp.com/api/quotes/random";
+var url = "https://thesimpsonsquoteapi.glitch.me/quotes";
 
 var colors = ['#2980b9', '#e74c3c', '#e67e22', '#27ae60', '#f1c40f', '#1abc9c', '#95a5a6', '#8e44ad', "#16a085", "#d35400", "#f39c12", "#3498db"];
 
 var getQuote = function (data) {
   $("#text").text(" " + data.quote);
   
-  if (data.author === '') {
-    data.author = 'Unknown';
+  if (data.character === '') {
+    data.character = 'Unknown';
   }
   
   $("#author").text("- " + data.author);
   
-var quot = 'https://twitter.com/intent/tweet?text=' + data.author + 'Author ' + data.quote +' @terrifictor codepen.io/terrifictor100';
+var quot = 'https://twitter.com/intent/tweet?text=' + data.character + 'Character ' + data.quote +' @terrifictor codepen.io/terrifictor100';
   
   $(".twitter-share").attr("href", quot);
   
